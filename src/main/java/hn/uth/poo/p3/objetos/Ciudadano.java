2119f0422af0e6a202fc85675abb7748dc5f1623
German Villanueva <xander_sn_91@hotmail.com>
2020-07-18 16:36:46 -0600

    Update Ciudadano.java

--- src/main/java/hn/uth/poo/p3/objetos/Ciudadano.java
+++ src/main/java/hn/uth/poo/p3/objetos/Ciudadano.java
@@ -10,5 +10,30 @@ package hn.uth.poo.p3.objetos;
  * @author Alexander
  */
 public class Ciudadano {
-    
+    private int Id;
+    private String Nomre;
+
+    public Ciudadano() {
+    }
+
+    public Ciudadano(int Id, String Nomre) {
+        this.Id = Id;
+        this.Nomre = Nomre;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNomre() {
+        return Nomre;
+    }
+
+    public void setNomre(String Nomre) {
+        this.Nomre = Nomre;
+    }
 }
